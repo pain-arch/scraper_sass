afcfdbb87dc96db5659ec8d79d620d8a5b348c8a
pain-arch <himulol123@gmail.com>
2025-01-29 16:40:52 +0600

    Add theme toggle functionality and breadcrumb header to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,22 +1,29 @@
-import DesktopSidebar from '@/components/Sidebar'
-import { Separator } from '@/components/ui/separator'
-import React from 'react'
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
+import DesktopSidebar from "@/components/Sidebar";
+import { ModeToggle } from "@/components/ThemeModeToggle";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
 
-const layout = ({children}:{children:React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex h-screen'>
-      <DesktopSidebar />  
-      <div className='flex flex-col flex-1 min-h-screen'>
-        <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>Scraper SASS</header>
+    <div className="flex h-screen">
+      <DesktopSidebar />
+      <div className="flex flex-col flex-1 min-h-screen">
+        <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+          <BreadcrumbHeader/>
+          <div className="gpa-1 flex items-center">
+            <ModeToggle />
+          </div>
+        </header>
         <Separator />
-        <div className='overflow-auto'>
-            <div className="flex-1 container py-4 text-accent-foreground">
-                {children}
-            </div>
+        <div className="overflow-auto">
+          <div className="flex-1 container py-4 text-accent-foreground">
+            {children}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
