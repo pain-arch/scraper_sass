1f11171c8ef2482588a456efa4a87d5d157c06e3
pain-arch <himulol123@gmail.com>
2025-01-29 18:30:55 +0600

    Add authentication layout and sign-in/sign-up pages with Clerk integration

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function layout({children}:{children:React.ReactNode}) {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-4'>
+      {children}
+    </div>
+  )
+}
+
+export default layout
