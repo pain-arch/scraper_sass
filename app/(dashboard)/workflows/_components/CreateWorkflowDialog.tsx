88dc4e23e7f6864a996ede210daf96049216d4c6
pain-arch <himulol123@gmail.com>
2025-01-30 19:55:48 +0600

    Add WorkflowCard component for displaying workflows and reset form on dialog close

--- app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -54,7 +54,10 @@ function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
 
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={(open) => {
+      form.reset();
+      setOpen(open);
+    }}>
       <DialogTrigger asChild>
         <Button>{triggerText ?? "Create workflow"}</Button>
       </DialogTrigger>
