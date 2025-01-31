1461762df46e9415aa0d64a9f95eddc9012215d0
pain-arch <himulol123@gmail.com>
2025-01-31 22:49:56 +0600

    Add FlowEditor component and CreateFlowNode utility for workflow node management

---
+++ app/workflow/_components/FlowEditor.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Workflow } from "@prisma/client";
+import React from "react";
+import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
+import { TaskType } from "@/types/task";
+
+function FlowEditor({workflow}: {workflow: Workflow}) {
+
+  const [nodes, setNodes, onNodesChange] = useNodesState([
+    CreateFlowNode(TaskType.LAUNCH_BROWSER)
+  ]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+
+  return (
+    <main className="h-full w-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
+      <Controls position="top-left" />
+      <Background variant={BackgroundVariant.Lines} gap={12} size={1}/>
+      </ReactFlow>
+    </main>
+  );
+}
+
+export default FlowEditor;
